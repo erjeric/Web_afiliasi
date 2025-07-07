85897e9fab1c602541d1ae64a72b9d6ec54ead2c
erjeric <jayamahendra32@gmail.com>
2025-07-07 17:37:33 +0700

    crose

--- tracker.js
+++ tracker.js
@@ -1,28 +1,32 @@
-// Tracking kunjungan halaman (index.html misalnya)
-fetch("https://script.google.com/macros/s/AKfycbw5K2IQDLdIsc0598ccX_5H5AwavzhTy-imbajTqWs/exec", {
+const proxy = "https://corsproxy.io/?";
+const apiUrl = "https://script.google.com/macros/s/AKfycbw5K2IQDLdIsc0598ccX_5H5AwavzhTy-imbajTqWs/exec";
+
+// Tracking kunjungan halaman
+fetch(proxy + apiUrl, {
   method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
   body: JSON.stringify({
     aksi: "kunjungan",
     halaman: "index.html",
     waktu: new Date().toISOString()
-  }),
-  headers: {
-    "Content-Type": "application/json"
-  }
+  })
 });
 
 // Tracking klik produk afiliasi
 function laporKlik(namaProduk) {
-  fetch("https://script.google.com/macros/s/AKfycbw5K2IQDLdIsc0598ccX_5H5AwavzhTy-imbajTqWs/exec", {
+  fetch(proxy + apiUrl, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({
       aksi: "klik_produk",
       nama: namaProduk,
       waktu: new Date().toISOString()
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  }).then(() => console.log("Klik dicatat:", namaProduk))
+    })
+  })
+    .then(() => console.log("Klik dicatat:", namaProduk))
     .catch(err => console.error("Gagal catat klik", err));
 }
